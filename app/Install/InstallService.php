5988ed1c90c88007207ebc6a5e0975c50a956e50
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-22 16:10:41 +0200

    build: validate LDAP runtime requirement

--- app/Install/InstallService.php
+++ app/Install/InstallService.php
@@ -16,7 +16,7 @@ final class InstallService
 
     public function requirements(): array
     {
-        $requiredExtensions = ['pdo', 'pdo_mysql', 'mbstring', 'openssl', 'dom', 'phar', 'curl'];
+        $requiredExtensions = ['pdo', 'pdo_mysql', 'mbstring', 'openssl', 'dom', 'phar', 'curl', 'ldap'];
         $extensions = [];
         foreach ($requiredExtensions as $extension) {
             $extensions[$extension] = extension_loaded($extension);
