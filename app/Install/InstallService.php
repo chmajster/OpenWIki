43a007a34eaf71556b35c4269c67190a0b8097ea
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-22 20:19:40 +0200

    build: validate ZIP runtime requirement

--- app/Install/InstallService.php
+++ app/Install/InstallService.php
@@ -16,7 +16,7 @@ final class InstallService
 
     public function requirements(): array
     {
-        $requiredExtensions = ['pdo', 'pdo_mysql', 'mbstring', 'openssl', 'dom', 'phar', 'curl', 'ldap'];
+        $requiredExtensions = ['pdo', 'pdo_mysql', 'mbstring', 'openssl', 'dom', 'phar', 'curl', 'ldap', 'zip'];
         $extensions = [];
         foreach ($requiredExtensions as $extension) {
             $extensions[$extension] = extension_loaded($extension);
