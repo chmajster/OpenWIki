ef4f7af0aae661acda35cf3c9be1bec5a314042b
chmajster <44300091+chmajster@users.noreply.github.com>
2026-09-22 14:58:28 +0200

    feat: register REST API v1 routes

--- routes/web.php
+++ routes/web.php
@@ -50,3 +50,17 @@ $router->post('/notifications/{id}/read', [NotificationController::class, 'read'
 $router->get('/account/api-tokens', [ApiTokenController::class, 'index']);
 $router->post('/account/api-tokens', [ApiTokenController::class, 'create']);
 $router->post('/account/api-tokens/{id}/revoke', [ApiTokenController::class, 'revoke']);
+
+$router->get('/api/v1/spaces', [ApiV1Controller::class, 'spaces']);
+$router->post('/api/v1/spaces', [ApiV1Controller::class, 'createSpace']);
+$router->get('/api/v1/pages', [ApiV1Controller::class, 'pages']);
+$router->post('/api/v1/pages', [ApiV1Controller::class, 'createPage']);
+$router->get('/api/v1/pages/{id}', [ApiV1Controller::class, 'page']);
+$router->put('/api/v1/pages/{id}', [ApiV1Controller::class, 'updatePage']);
+$router->patch('/api/v1/pages/{id}', [ApiV1Controller::class, 'updatePage']);
+$router->delete('/api/v1/pages/{id}', [ApiV1Controller::class, 'deletePage']);
+$router->get('/api/v1/search', [ApiV1Controller::class, 'search']);
+$router->get('/api/v1/users', [ApiV1Controller::class, 'users']);
+$router->get('/api/v1/groups', [ApiV1Controller::class, 'groups']);
+$router->get('/api/v1/tags', [ApiV1Controller::class, 'tags']);
+$router->get('/api/v1/attachments', [ApiV1Controller::class, 'attachments']);
